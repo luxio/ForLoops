bec539e4e1715c7ef3a22f0e88026b7bc650f80b
Stephane Lux <stephane@lux.io>
2014-09-25 18:53:39 +0200

    Initial Commit

---
+++ ForLoops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForLoops
+//
+//  Created by Stéphane Lux on 25.09.14.
+//  Copyright (c) 2014 LUXio IT-Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
